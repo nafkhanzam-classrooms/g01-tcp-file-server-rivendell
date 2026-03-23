d4f5b81bc0582b86f6384a9e56c685a87f2f389d
nittsuj <justinvalentino88@gmail.com>
2026-03-23 22:32:14 +0700

    feat: basic echo and /list command

--- server-sync.py
+++ server-sync.py
@@ -1,11 +1,17 @@
 import socket
+import os
 
 HOST = '127.0.0.1'
 PORT = 8888
 
+
 ADDR = (HOST, PORT)
 
+FILE_DIR = 'server_folder'
+
 BUFF_SIZE = 1024
+
+if not os.path.exists(FILE_DIR): os.makedirs(FILE_DIR)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -22,5 +28,12 @@ while True:
         while True:
             data = connection.recv(BUFF_SIZE)
             if not data: break
-            connection.sendall(data)
+            lines = data.decode().split('\n')
+            for li in lines:
+                if not li: continue
+                if li.startswith('/list'):
+                    files = os.listdir(FILE_DIR)
+                    response = '\n'.join(files) + '\n'
+                    connection.sendall(response.encode())
+                else: connection.sendall(data)
     print(f"{address} disconnected")
